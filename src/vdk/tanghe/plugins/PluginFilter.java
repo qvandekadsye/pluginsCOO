5b11653b8283d3c2266363c8bfa759499b87c305
qvandekadsye <qvandekadsye@gmail.com>
2014-12-03 11:14:41 +0100

    modifying PluginFilter

--- src/vdk/tanghe/plugins/PluginFilter.java
+++ src/vdk/tanghe/plugins/PluginFilter.java
@@ -1,5 +1,24 @@
 package vdk.tanghe.plugins;
+import java.io.File;
 
-public class PluginFilter {
+public class PluginFilter implements java.io.FilenameFilter{
+	
+	private PluginFilter()
+	{
+		
+	}
+	
+	public PluginFilter getInstance()
+	{
+		return new PluginFilter();
+	}
+
+	@Override
+	public boolean accept(File arg0, String name) {
+		
+		
+		return name.endsWith(".class");
+	}
+	
 
 }
